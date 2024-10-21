a8377a3b454f62575caea507278c088823bc8eb6
DeivisonSantana <deivisonssouza@hotmail.com>
2024-10-21 13:34:35 -0400

    inicial commit

---
+++ statics/script.js
@@ -0,0 +1,15 @@
+function redirectToCheckout() {
+    window.open('https://pay.kiwify.com.br/umvXfs8?afid=vobS46hG', '_blank')
+}
+function redirectToTiktok() {
+    window.open('https://www.tiktok.com/@atitudeatletica', '_blank')
+}
+function redirectToInstagram() {
+    window.open('https://www.instagram.com/atitudeatleticaof', '_blank')
+}
+function redirectToYoutube() {
+    window.open('https://www.youtube.com/channel/UCaEnl8QQnd8G1e3qlXP-L4g', '_blank')
+}
+function redirectToKwai() {
+    window.open('https://www.kwai.com/@atitudeatletica', '_blank')
+}
